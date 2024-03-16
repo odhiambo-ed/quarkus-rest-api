e353b85fc6cb3db949b20b5ee7549d262d11d0f4
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-03-16 18:15:43 +0300

    add project entity

--- src/main/java/com/edward/fullstack/project/Project.java
+++ src/main/java/com/edward/fullstack/project/Project.java
@@ -1,4 +1,28 @@
 package com.edward.fullstack.project;
 
-public class Project {
+import com.edward.fullstack.user.User;
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "projects", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"name", "user_id"})
+})
+public class Project extends PanacheEntity {
+    @Column(nullable = false)
+    public String name;
+
+    @ManyToOne(optional = false)
+    public User user;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    public ZonedDateTime created;
+
+    @Version
+    public int version;
+
 }
